3a4d3253f30c8cb129f647fe734cdb6260b74d5c
EndersWitch <joshua.jenner2000@gmail.com>
2026-04-14 14:05:18 +0200

    Add files via upload

---
+++ api/admin-auth.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler
+import json, os
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        length = int(self.headers.get('Content-Length', 0))
+        body = json.loads(self.rfile.read(length))
+        pw = body.get('password', '')
+        correct = os.environ.get('ADMIN_PASSWORD', '')
+        if pw and correct and pw == correct:
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({'ok': True}).encode())
+        else:
+            self.send_response(401)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({'ok': False}).encode())
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+    def log_message(self, *a): pass
